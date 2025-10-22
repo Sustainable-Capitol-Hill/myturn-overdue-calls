de4e1f633ed32a83a374a79a7d52ae85f09f7247
Miles Watkins <miles.w.watkins@gmail.com>
2025-10-21 23:25:33 -0700

    Initial GitHub publish

---
+++ MakeCalls.js
@@ -0,0 +1,120 @@
+// Note: have to use ES5(?) syntax/polyfills since we deactivated the V8 JavaScript engine for this project
+// See discussion on https://stackoverflow.com/questions/60230776/were-sorry-a-server-error-occurred-while-reading-from-storage-error-code-perm
+
+function onOpen() {
+  SpreadsheetApp
+    .getUi()
+    .createMenu('Make Calls')
+    .addItem('Initiate call', 'initiateCallDialog')
+    .addItem('Initiate call for specific item', 'initiateFilteredCallDialog')
+    .addToUi();
+}
+
+var CALL_OUTCOME_CATEGORIES = {
+  TALKED_WILL_RETURN: '🗣️ Talked to patron, and they plan to return the item(s)',
+  TALKED_WONT_RETURN: "🗣️ Talked to patron, but they won't or can't return the item(s)",
+  TALKED_DROPPED: '🗣️ Talked to patron, but they hung up on you or the call dropped',
+  TALKED_NOT_CHECKED_OUT: '🗣️ Talked to patron, but they already returned (or never checked out) the item(s); please correct this in MyTurn!',
+  VOICEMAIL_LEFT: '☎️ Successfully left a voicemail',
+  VOICEMAIL_FULL: '📵 Voicemail full or not functioning',
+  FAILED_DISCONNECTED: '📵 Phone number is disconnected or out of service',
+  FAILED_WRONG_NUMBER: '📵 Phone number appears to be associated with a different person',
+  FAILED_INVALID_NUMBER: '📵 Phone number is incomplete, invalid, or missing',
+}
+
+function getRandomItemFromArray(items) {
+  return items[Math.floor(Math.random() * items.length)]
+}
+
+function parseRowsIntoObjects(headers, rows) {
+  return rows.map(function (row) {
+    const rowObject = {}
+
+    row.map(function (value, index) {
+      const header = headers[index]
+      rowObject[header] = value
+    })
+
+    return rowObject
+  })
+}
+
+function processCallForm(formObject) {
+  insertCallsMadeRow(
+    // This should be in the same time zone as the user's spreadsheet (so almost always Pacific Time)
+    (new Date()).toLocaleDateString('en-US'),
+    formObject.username,
+    formObject.outcomeCategory,
+    formObject.outcomeNotes
+  )
+}
+
+function insertCallsMadeRow(date, username, outcomeCategory, outcomeNotes) {
+  const callsSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Calls Made")
+  const callsColumnCount = callsSheet.getMaxColumns()
+  callsSheet.insertRowAfter(1)
+  callsSheet.getRange(2, 1, 1, callsColumnCount)
+    .setValues([[
+      date,
+      username,
+      outcomeCategory,
+      outcomeNotes
+    ]])
+}
+
+function getAllOverdueUsers() {
+  const usersToCallSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Users To Call")
+  const usersToCallColumnCount = usersToCallSheet.getMaxColumns()
+  const usersToCallHeaders = usersToCallSheet.getRange(1, 1, 1, usersToCallColumnCount).getValues()[0]
+  const usersToCallRows = usersToCallSheet.getRange(
+    2,
+    1,
+    usersToCallSheet.getMaxRows() - 1,
+    usersToCallColumnCount
+  ).getValues().filter(function (i) { return Boolean(i[0]) })
+  const usersToCall = parseRowsIntoObjects(usersToCallHeaders, usersToCallRows)
+
+  return usersToCall
+}
+
+function initiateCallDialog(filters) {
+  // Set filter defaults
+  const minimumDaysOverdue = (filters && typeof filters.minimumDaysOverdue === "number") ? filters.minimumDaysOverdue : 45
+  const minimumDaysSinceLastCall = (filters && typeof filters.minimumDaysSinceLastCall === "number") ? filters.minimumDaysSinceLastCall : 14
+  const itemNameToSearch = filters && filters.itemNameToSearch
+
+  const usersToCall = getAllOverdueUsers()
+    .filter(function (user) {
+      return new Date(new Date(user['latest due date']).getTime() + minimumDaysOverdue * 24 * 60 * 60 * 1000) < new Date()
+    })
+    .filter(function (user) {
+      // Has never been called or…
+      return !user['most recent call'] ||
+        // …hasn't been called in at least the past X days
+        // Source: https://stackoverflow.com/questions/7687884/add-10-seconds-to-a-date
+        new Date(new Date(user['most recent call']).getTime() + minimumDaysSinceLastCall * 24 * 60 * 60 * 1000) < new Date()
+    })
+    .filter(function (user) {
+      return !itemNameToSearch ||
+        user['overdue item names'].toLowerCase().indexOf(itemNameToSearch) > -1
+    })
+    .filter(function (user) {
+      return !user['should mark items as lost by member?']
+    })
+  
+  if (usersToCall.length === 0) {
+    return SpreadsheetApp.getUi().alert("No calls need to be made right now");
+  }
+
+  const userToCall = getRandomItemFromArray(usersToCall)
+
+  const t = HtmlService.createTemplateFromFile("callForm")
+  t.outcomeCategories = CALL_OUTCOME_CATEGORIES
+  t.userToCall = userToCall
+  SpreadsheetApp.getUi().showModalDialog(t.evaluate(), "Overdue call to make")
+}
+
+function initiateFilteredCallDialog() {
+  const t = HtmlService.createHtmlOutputFromFile('callFilterForm')
+  SpreadsheetApp.getUi().showModalDialog(t, "Filter your overdue call")
+}
---
+++ UpdateMyTurnData.js
@@ -0,0 +1,83 @@
+function updateMyTurnSheetsData() {
+  const sessionIdCookie = getAuthenticatedSessionCookie()
+
+  const overdueSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("MyTurn Report: Loans, Overdue Only")
+  // This appears to be an internal MyTurn customer ID for our organization
+  const locationId = '66'
+  const overdueResponse = UrlFetchApp.fetch(
+    'https://capitolhill.myturn.com/library/orgLoan/exportLoans',
+    {
+      method: 'post',
+      payload: {
+        dueBefore: "struct",
+        // `toLocaleDateString` doesn't handle its `options` correctly in this old JS engine,
+        // so brute-force formatting the date. We also can't control which time zone this
+        // is run on, and old JS versions (like this one) don't support passing a time zone
+        // into stringification, so we just have to set everything in UTC.
+        dueBefore_date: (new Date().getUTCMonth() + 1).toString() + '/' + (new Date().getUTCDate() - 1).toString() + '/' + new Date().getUTCFullYear().toString(),
+        dueBefore_time: new Date().getUTCHours() + ':' + new Date().getUTCMinutes().toString(),
+        dueBefore_tz: 'UTC',
+        "location.id": locationId,
+        out: "true",
+        format: "csv"
+      },
+      headers: {
+        Cookie: sessionIdCookie
+      }
+    }
+  )
+  const overdueData = Utilities.parseCsv(overdueResponse.getContentText())
+  overdueSheet.clear()
+  overdueSheet.getRange(1, 1, overdueData.length, overdueData[0].length)
+    .setValues(overdueData)
+
+  const checkedOutSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("MyTurn Report: Inventory, List Items, Checked Out")
+  const checkedOutResponse = UrlFetchApp.fetch(
+    'https://capitolhill.myturn.com/library/orgInventory/exportItemList',
+    {
+      method: 'post',
+      payload: {
+        availability: 'checked_out',
+        format: 'csv'
+      },
+      headers: {
+        Cookie: sessionIdCookie
+      }
+    }
+  )
+  const checkedOutData = Utilities.parseCsv(checkedOutResponse.getContentText())
+  checkedOutSheet.clear()
+  checkedOutSheet.getRange(1, 1, checkedOutData.length, checkedOutData[0].length)
+    .setValues(checkedOutData)
+}
+
+function getAuthenticatedSessionCookie() {
+  const userProperties = PropertiesService.getUserProperties();
+  // These secrets are stored as "user properties", and are therefore accessible
+  // only to the Google Account who initially saved/populated them. (There's not
+  // a dramatically better way to manage secrets on a shared Google Sheet/Apps
+  // Script like this.)
+  // https://developers.google.com/apps-script/guides/properties
+  const username = userProperties.getProperty('myturn-username');
+  const password = userProperties.getProperty('myturn-password');
+
+  const loginResponse = UrlFetchApp.fetch(
+    'https://capitolhill.myturn.com/library/j_spring_security_check',
+    {
+      method: 'post',
+      payload: {
+        j_username: username,
+        j_password: password
+      },
+      followRedirects: false
+    }
+  )
+  const sessionIdCookie = loginResponse.getAllHeaders()['Set-Cookie']
+    .split(';')
+    .map(function (i) { return i.trim(); })
+    // Gosh, not having ES6 support is annoying; I miss `startsWith`
+    .filter(function (i) { return i.slice(0, 11) === 'JSESSIONID='; })
+    [0]
+
+  return sessionIdCookie
+}
